681b943f87e92b8b6f521d6bf481a5218fd5962f
Naif Ghannam <dhdfaui@gmail.com>
2025-03-25 11:23:31 +0300

    Initial Commit

---
+++ TestBase/TestBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBaseApp.swift
+//  TestBase
+//
+//  Created by Mac on 25/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TestBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
